65b62d7ee01913c92feb7a758657d188943dd540
Olivia Pomeroy <opomeroy26@gmail.com>
2022-05-23 16:53:38 -0700

    Solved JavaScript solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,17 @@
 function reverseString(str) {
   // type your code here
+  // set an empty string, get the last letter and return it as the first
+
+  let reversed = ""
+
+  for (let i = str.length - 1; i > -1; --i ) {
+    reversed = reversed + str[i] ;
+  }
+
+  return reversed;
 }
+
+
 
 if (require.main === module) {
   // add your own tests in here
